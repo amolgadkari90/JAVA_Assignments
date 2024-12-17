e0ccdaaf0ff13dde3bf3ab9aff02abb9a99164ec
Amol <amolgadkari90@gmail.com>
2024-12-17 13:30:01 +0530

    Add files via upload

---
+++ 10_CW_Inheritance/src/com/tka/dec16/A.java
@@ -0,0 +1,17 @@
+package com.tka.dec16;
+
+public class A 
+{
+	int a = 10;
+	int b = 100;
+	//String b = " ";
+	
+	A()
+	{
+		
+	}
+	public void m1()
+	{
+		System.out.println("111");
+	}
+}
---
+++ 10_CW_Inheritance/src/com/tka/dec16/B.java
@@ -0,0 +1,16 @@
+package com.tka.dec16;
+
+public class B extends A
+{
+	String a = "TKA";
+	B(String s)
+	{
+		a = s;
+	
+	}
+	
+	public void m1()
+	{
+		System.out.println("222");
+	}
+}
